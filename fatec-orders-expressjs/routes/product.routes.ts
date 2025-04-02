ad73b2dbec6c472c9816423b5e36a7d9bd57ab46
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-04-01 22:06:20 -0300

    adiciona rotas para gerenciamento de produtos com filtragem e busca por ID

---
+++ fatec-orders-expressjs/routes/product.routes.ts
@@ -0,0 +1,78 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import { IProductFilterOptions } from '../interfaces/IProduct';
+
+const router = express.Router();
+
+
+const products = [
+    {
+        id: 1,
+        name: "Feijão Carioca",
+        brand: "Broto Legal",
+        barCode: "98493874849278374989478",
+        supplier: "Rede de Distribuição Ltda",
+        stockId: 98,
+        price: 8.79,
+        weight: 1,
+        measureUnit: "kg",
+    },
+    {
+        id: 2,
+        name: "Arroz",
+        brand: "Tio João",
+        barcode: "98475834587947857947984",
+        supplier: "Rede de Distribuição Ltda",
+        stockId: 65,
+        price: 29.99,
+        weight: 5,
+        measureUnit: "kg",
+    },
+];
+
+// Define método Http Get que responde no path /product/:id
+router.get("/product/:id", (req: Request, res: Response) => {
+    const product = products.find((product) => {
+        return product.id === Number(req.params.id);
+    });
+
+    if (!product) {
+        res.status(404).send();
+        return;
+    }
+
+    // Responde requisição com o produto encontrado
+    res.status(200).json(product);
+});
+
+// Define método Http Get que responde no path /product
+router.get("/product", (req: Request, res: Response) => {
+    const productFilter = req.query as unknown as IProductFilterOptions;
+
+    const {
+        name: nameFilter,
+        brand: brandFilter,
+        supplier: supplierFilter,
+        stockId: stockIdFilter
+    } = productFilter;
+
+    const foundProducts = products.filter(({ name, brand, supplier, stockId }) => {
+        return (
+            (!nameFilter || name.toUpperCase().includes(nameFilter.toUpperCase())) &&
+            (!brandFilter || brand.toUpperCase().includes(brandFilter.toUpperCase())) &&
+            (!supplierFilter || supplier.toUpperCase().includes(supplierFilter.toUpperCase())) &&
+            (!stockIdFilter || stockId === Number(stockIdFilter))
+        );
+    }
+    );
+
+    res.status(200).json(foundProducts);
+});
+
+router.post("/product",
+    (req: Request, res: Response) => {
+        const product = req.body;
+        products.push(product);
+
+        res.status(201).send();
+    });
