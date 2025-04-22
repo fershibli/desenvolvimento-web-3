a86e68cad6cdf73acb15de8f81b2293ab3a391fa
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-04-22 20:39:02 -0300

    feat(routes): adiciona rota para o controlador de marcas

--- fatec-orders-expressjs/app.ts
+++ fatec-orders-expressjs/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import ProductRoutes from "./src/routes/product.routes";
+import BrandRoutes from "./src/routes/brand.routes";
 
 // Criação da aplicação
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 // Routes
 app.use("/product", ProductRoutes);
+app.use("/brand", BrandRoutes);
 
 // Inicia aplicação na Porta 3000
 app.listen(3000, () => {
