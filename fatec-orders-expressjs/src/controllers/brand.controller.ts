1e296d922563632199f6a3718af6dd8bb9f9585b
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-04-22 20:40:57 -0300

    feat(controller): adiciona tipagem ao retorno da função listAll

--- fatec-orders-expressjs/src/controllers/brand.controller.ts
+++ fatec-orders-expressjs/src/controllers/brand.controller.ts
@@ -1,6 +1,7 @@
+import { Brand } from "../interfaces/brand.interface";
 import { BrandModel } from "../models/brand.models";
 
-export const listAll = async () => {
+export const listAll = async (): Promise<Brand[]> => {
     const brands = await BrandModel.findAll();
     return brands;
 }
