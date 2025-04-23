b442814b46b8f60ef0d6a757c00aefbddd96d69a
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-04-22 21:36:52 -0300

    feat(routes): adiciona função de criação de marca na rota

--- fatec-orders-expressjs/src/routes/brand.routes.ts
+++ fatec-orders-expressjs/src/routes/brand.routes.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { listAll } from '../controllers/brand.controller';
+import { create, listAll } from '../controllers/brand.controller';
 
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
     const allBrands = await listAll();
     res.status(200).json(allBrands);
+})
+
+router.post("/", async (req: Request, res: Response) => {
+    const brand = req.body;
+    const newBrand = await create(brand.description);
+    res.status(201).json(newBrand);
 })
 
 export default router;
