cb2a12adc28fc81bff4d9082bb7183efe2c81aec
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-13 20:45:29 -0300

    feat(routes): adiciona middleware de log e validação na criação de marca

--- fatec-orders-expressjs/src/routes/brand.routes.ts
+++ fatec-orders-expressjs/src/routes/brand.routes.ts
@@ -1,15 +1,27 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { create, listAll } from '../controllers/brand.controller';
 
 const router = express.Router();
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log("LOGGED");
+    next();
+}
+
+const createBrandMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    console.log("Descrição: " + req.body.description);
+    next();
+}
+
+router.use(logger);
 
 router.get("/", async (req: Request, res: Response) => {
     const allBrands = await listAll();
     res.status(200).json(allBrands);
 })
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/", createBrandMiddleware, async (req: Request, res: Response) => {
     const { description } = req.body;
     const newBrand = await create(description);
     res.status(201).json(newBrand);
