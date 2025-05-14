78d1c31d2250c3a09e9c6b990a7bb2616fc8c76b
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-13 22:02:50 -0300

    feat(auth): retorna token JWT na autenticação bem-sucedida

--- fatec-orders-expressjs/src/routes/auth.routes.ts
+++ fatec-orders-expressjs/src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const AuthRouter = express.Router();
 
 AuthRouter.post("/", async (req: Request, res: Response) => {
     const { username, password } = req.body;
     if (username === "admin" && password === "admin") {
-        res.status(200).send();
+        const token = jwt.sign({ username }, "s3nh4S3gur4")
+        res.status(200).json({ token });
     }
     res.status(401).send();
 })
