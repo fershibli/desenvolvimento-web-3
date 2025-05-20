e08da83109507c5a2154927c3585917666d5c50d
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-20 20:09:09 -0300

    feat(middleware): adiciona configuração do dotenv no middleware de autorização

--- fatec-orders-expressjs/src/middlewares/authorize.middleware.ts
+++ fatec-orders-expressjs/src/middlewares/authorize.middleware.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AuthorizeMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const { authorization } = req.headers;
